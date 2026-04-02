0ac4353e8ffa302035ce593ac4a6bc1496316443
AaronATM <aaronkrapes@outlook.com>
2026-04-02 05:28:14 +0000

    changed networking displays to use modified psutils function

--- route/health.py
+++ route/health.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 import psutil
 import subprocess
+# custom netutils file with modified psutil functions
+from .netutils import net_io_counters
 
 router = APIRouter()
-
 
 def get_gpu():
     try:
@@ -34,13 +35,29 @@ def get_gpu():
         pass
     return None
 
-
 @router.get('/health')
 async def getHealth():
     cpu = psutil.cpu_percent(interval=0.5)
     mem = psutil.virtual_memory()
     disk = psutil.disk_usage('/')
-    net = psutil.net_io_counters()
+    net = net_io_counters()
+    # net now has bytes sent and recv data for all interfaces
+    # i.e. {'lo': (45814314, 45814314, 220424, 220424, 0, 0, 0, 0), 'enp1s0': (17252702, 106527039, 68781, 91602, 0, 0, 72, 0)...}
+    # so if you want to get the info for the main interface regardless of name then the best way is to
+    # convert the values to a list and use index 1 (0 is loopback/localhost) like this: list(net.values())[1]
+    # bytes sent is the nested 0 index, and bytes recieved is the nested 1 index
+    bytes_sent = list(net.values())[1][0]
+    bytes_recv = list(net.values())[1][1]
+    # you could also use a loop to sum all interfaces, I will save it since I already wrote it:
+    '''
+    total_bytes_sent = 0
+    total_bytes_recv = 0
+    for interface, stats in net.items():
+        if interface=='lo': continue # skip localhost traffic
+        total_bytes_sent += stats[0]
+        total_bytes_recv += stats[1]
+    '''
+
     gpu = get_gpu()
 
     return {
@@ -58,10 +75,10 @@ async def getHealth():
             'percent': disk.percent,
         },
         'network': {
-            'bytes_sent': net.bytes_sent,
-            'bytes_recv': net.bytes_recv,
-            'mb_sent': round(net.bytes_sent / 1e6, 1),
-            'mb_recv': round(net.bytes_recv / 1e6, 1),
+            'bytes_sent': bytes_sent,
+            'bytes_recv': bytes_recv,
+            'mb_sent': round(bytes_sent / 1e6, 1),
+            'mb_recv': round(bytes_recv / 1e6, 1),
         },
         'gpu': gpu,
     }
